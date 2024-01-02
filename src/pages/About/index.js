0ee103cbd258de2007d48ad182480eeb09d6b8c1
phoebus9x <anhtu.coder0512@gmail.com>
2024-01-02 22:44:58 +0300

    My message

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,22 @@
 import "../Home/Home.scss";
+import "./Reponsive.scss";
+
+import { useTranslation } from "react-i18next";
+import "aos/dist/aos.css";
+import AOS from "aos";
+
+import images from "~/assets/images";
+import Navbar from "~/Layout/components/Navbar";
+import Button from "~/components/Button";
 
 function About() {
-  return <div>About Page</div>;
+  const { t } = useTranslation("home");
+  // {t("key")}
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return <div class="site-wrapper"></div>;
 }
 
 export default About;
