781e758d86c48a5e121e56ac966c9fb47555dcb1
phoebus9x <anhtu.coder0512@gmail.com>
2024-01-21 14:16:06 +0300

    update 21.01

--- src/Layout/components/Footer/index.js
+++ src/Layout/components/Footer/index.js
@@ -1,87 +1,81 @@
-import classNames from "classnames/bind";
-import styles from "./Footer.module.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faPhone } from "@fortawesome/free-solid-svg-icons";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import Button from "~/components/Button";
-import { Link } from "react-router-dom";
-import images from "~/assets/images";
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import Button from '~/components/Button';
+import { Link } from 'react-router-dom';
+import images from '~/assets/images';
+
+import { useTranslation } from 'react-i18next';
+import { locales } from '~/i18n/i18n';
 
 const cx = classNames.bind(styles);
 
 function Footer() {
-  return (
-    <footer>
-      <div className={cx("container")}>
-        <div className={cx("row justify-content-between")}>
-          <div className={cx("col-lg-2 col-md-3 col-sm-6")}>
-            <div>
-              <h4>Quick Links</h4>
-              <div className={cx("footer-links", "d-flex flex-column")}>
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Destination</a>
-                <a href="">Contact</a>
-              </div>
-            </div>
-          </div>
-          <div className={cx("col-lg-2 col-md-3 col-sm-6")}>
-            <div>
-              <h4>Contact Info</h4>
-              <div className={cx("address")}>
-                <div>
-                  <FontAwesomeIcon className={cx("icon")} icon={faHouse} />
-                  <p>Xã An Ninh, huyện Quỳnh Phụ, Tỉnh Thái Bình</p>
+    const { i18n } = useTranslation();
+    const { t } = useTranslation('home');
+    const currentLanguage = locales[i18n.language];
+
+    return (
+        <footer>
+            <div className={cx('container')}>
+                <div className={cx('row justify-content-between')}>
+                    <div className={cx('col-lg-2 col-md-3 col-sm-6')}>
+                        <div>
+                            <h4>{t('quick links')}</h4>
+                            <div className={cx('footer-links', 'd-flex flex-column')}>
+                                <a href="">{t('home')}</a>
+                                <a href="">{t('about')}</a>
+                                <a href="">{t('destination')}</a>
+                                <a href="">{t('contact')}</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('col-lg-2 col-md-3 col-sm-6')}>
+                        <div>
+                            <h4>{t('contact info')}</h4>
+                            <div className={cx('address')}>
+                                <div>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faHouse} />
+                                    <p>Xã An Ninh, huyện Quỳnh Phụ, Tỉnh Thái Bình</p>
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faEnvelope} />
+                                    <p>Tu051220@gmail.com</p>
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon className={cx('icon')} icon={faPhone} />
+                                    <p>+375-25-513-89-93</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('col-md-5 col-md-5')}>
+                        <div className={cx('reservation')}>
+                            <h4>{t('make a reservation')}</h4>
+                            <p>{t('our support & sales team is available 24/7 to answer your queries')}</p>
+                            <div className={cx('justify-content-start mt-lg-4 mt-4')}>
+                                <Button>{t('book now')}</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className={cx('sub-footer', 'row justify-content-between align-items-center')}>
+                        <div className={cx('col-md-5')}>
+                            <Link to="/" className="header-link navbar-brand position-relative p-0">
+                                <img className="logo-img" src={images.logo} alt="Logo" />
+                            </Link>
+                        </div>
+                        <div className={cx('col-md-6')}>
+                            <p>{t('copyright © 2023 viettravel by datu. all rights reserved.')}</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                  <FontAwesomeIcon className={cx("icon")} icon={faEnvelope} />
-                  <p>Tu051220@gmail.com</p>
-                </div>
-                <div>
-                  <FontAwesomeIcon className={cx("icon")} icon={faPhone} />
-                  <p>+375-25-513-89-93</p>
-                </div>
-              </div>
             </div>
-          </div>
-          <div className={cx("col-md-5 col-md-5")}>
-            <div className={cx("reservation")}>
-              <h4>Make A Reservation</h4>
-              <p>
-                Our Support & Sales team is available 24/7 to answer your
-                queries
-              </p>
-              <div className={cx("justify-content-start mt-lg-4 mt-4")}>
-                <Button>Book Now</Button>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div
-            className={cx(
-              "sub-footer",
-              "row justify-content-between align-items-center"
-            )}
-          >
-            <div className={cx("col-md-5")}>
-              <Link
-                to="/"
-                className="header-link navbar-brand position-relative p-0"
-              >
-                <img className="logo-img" src={images.logo} alt="Logo" />
-              </Link>
-            </div>
-            <div className={cx("col-md-6")}>
-              <p>
-                Copyright © 2023 Viettravel By Do Anh Tu. All Rights Reserved.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 }
 
 export default Footer;
--- src/Layout/components/Navbar/index.js
+++ src/Layout/components/Navbar/index.js
@@ -1,170 +1,164 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Navbar.scss";
-import images from "~/assets/images";
-import Button from "~/components/Button";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.scss';
+import images from '~/assets/images';
+import Button from '~/components/Button';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-import { locales } from "~/i18n/i18n";
+import { locales } from '~/i18n/i18n';
 // className=)}
 function Navbar() {
-  const location = useLocation();
-  const { i18n } = useTranslation();
-  const { t } = useTranslation("home");
-  const currentLanguage = locales[i18n.language];
+    const location = useLocation();
+    const { i18n } = useTranslation();
+    const { t } = useTranslation('home');
+    const currentLanguage = locales[i18n.language];
 
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-  };
-  return (
-    <div className="header-content">
-      <header>
-        <nav className="navbar container navbar-expand-lg">
-          <div className="container-fluid">
-            <Link
-              to="/"
-              className="header-link navbar-brand position-relative p-0"
-            >
-              <img className="logo-img" src={images.logo} alt="Logo" />
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              {/* <FontAwesomeIcon
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+    };
+    return (
+        <div className="header-content">
+            <header>
+                <nav className="navbar container navbar-expand-lg">
+                    <div className="container-fluid">
+                        <Link to="/" className="header-link navbar-brand position-relative p-0">
+                            <img className="logo-img" src={images.logo} alt="Logo" />
+                        </Link>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            {/* <FontAwesomeIcon
                   className="navbar-toggler-icon"
                   icon={faBars}
                 /> */}
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav position-relative ms-auto mx-4 mb-2 mb-lg-0 d-flex gap-4 justify-content-center align-items-center">
-                <li className="nav-item">
-                  <Link
-                    className={`header-link nav-link ${location.pathname === '/' ? 'active' : ''}`}
-                    aria-current="page"
-                    to="/"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={`header-link nav-link ${location.pathname === '/about' ? 'active' : ''}`} to="/about">
-                    About
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={`header-link nav-link ${location.pathname === '/blog' ? 'active' : ''}`} to="/blog">
-                    Blog
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={`header-link nav-link ${location.pathname === '/contact' ? 'active' : ''}`} to="/contact">
-                    Contact
-                  </Link>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link
-                    className="header-link nav-link dropdown-toggle"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    {currentLanguage}
-                  </Link>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <div
-                        className="dropdown-item"
-                        onClick={() => changeLanguage("en")}
-                      >
-                        English
-                      </div>
-                    </li>
-                    <li>
-                      <div
-                        className="dropdown-item"
-                        onClick={() => changeLanguage("vi")}
-                      >
-                        Vietnamese
-                      </div>
-                    </li>
-                    <li>
-                      <div
-                        className="dropdown-item"
-                        onClick={() => changeLanguage("ru")}
-                      >
-                        Russian
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item dropdown">
-                  <Link
-                    className="header-link nav-link dropdown-toggle"
-                    to="/"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Pages
-                  </Link>
-                  <ul className="dd-menu dropdown-menu fade-up">
-                    <li>
-                      <Link className="dd-item dropdown-item" to="/Services">
-                        Services
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="dd-item dropdown-item"
-                        to="/Destinations"
-                      >
-                        Destinations
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="dd-item dropdown-item"
-                        to="/Testimonials"
-                      >
-                        Testimonials
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dd-item dropdown-item" to="/FAQ">
-                        FAQ
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dd-item dropdown-item" to="/Team">
-                        Team
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dd-item dropdown-item" to="/ComingSoon">
-                        Coming Soon
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                {/* <li className="nav-item")}>
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav position-relative ms-auto mx-4 mb-2 mb-lg-0 d-flex gap-4 justify-content-center align-items-center">
+                                <li className="nav-item">
+                                    <Link
+                                        className={`header-link nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                                        aria-current="page"
+                                        to="/"
+                                    >
+                                        {t('home')}
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className={`header-link nav-link ${
+                                            location.pathname === '/about' ? 'active' : ''
+                                        }`}
+                                        to="/about"
+                                    >
+                                        {t('about')}
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className={`header-link nav-link ${
+                                            location.pathname === '/blog' ? 'active' : ''
+                                        }`}
+                                        to="/blog"
+                                    >
+                                        {t('blog')}
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className={`header-link nav-link ${
+                                            location.pathname === '/contact' ? 'active' : ''
+                                        }`}
+                                        to="/contact"
+                                    >
+                                        {t('contact')}
+                                    </Link>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <Link
+                                        className="header-link nav-link dropdown-toggle"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        {currentLanguage}
+                                    </Link>
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <div className="dropdown-item" onClick={() => changeLanguage('en')}>
+                                                English
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="dropdown-item" onClick={() => changeLanguage('vi')}>
+                                                Vietnamese
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div className="dropdown-item" onClick={() => changeLanguage('ru')}>
+                                                Russian
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <Link
+                                        className="header-link nav-link dropdown-toggle"
+                                        to="/"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        {t('pages')}
+                                    </Link>
+                                    <ul className="dd-menu dropdown-menu fade-up">
+                                        <li>
+                                            <Link className="dd-item dropdown-item" to="/Services">
+                                                {t('services')}
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="dd-item dropdown-item" to="/Destinations">
+                                                {t('destinations')}
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="dd-item dropdown-item" to="/Testimonials">
+                                                {t('testimonials')}
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="dd-item dropdown-item" to="/FAQ">
+                                                {t('faq')}
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="dd-item dropdown-item" to="/Team">
+                                                {t('team')}
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link className="dd-item dropdown-item" to="/ComingSoon">
+                                                Coming Soon
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </li>
+                                {/* <li className="nav-item")}>
                     <a className="nav-link disabled")} aria-disabled="true">
                       Disabled
                     </a>
                   </li> */}
-              </ul>
-              <Button to="">{t("about us.book now")}</Button>
-              {/* <form className="d-flex")} role="search">
+                            </ul>
+                            <Button to="">{t('book now')}</Button>
+                            {/* <form className="d-flex")} role="search">
                   <input
                     className="form-control me-2")}
                     type="search"
@@ -178,13 +172,13 @@ function Navbar() {
                     Search
                   </button>
                 </form> */}
-            </div>
-          </div>
-        </nav>
-        <hr />
-      </header>
-    </div>
-  );
+                        </div>
+                    </div>
+                </nav>
+                <hr />
+            </header>
+        </div>
+    );
 }
 
 export default Navbar;
