9f76aa7281cbfa870454d6785cc411418dea6d91
phoebus9x <anhtu.coder0512@gmail.com>
2024-01-27 21:14:09 +0300

    update 27.01

---
+++ src/pages/SingleTour/index.js
@@ -0,0 +1,61 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useState, useEffect } from 'react';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+import '~/components/css/All.scss';
+import '~/components/css/Reponsive.scss';
+import images from '~/assets/images';
+import Navbar from '~/Layout/components/Navbar';
+import Button from '~/components/Button';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesRight, faArrowRight, faPlay, faStar } from '@fortawesome/free-solid-svg-icons';
+
+function SingleTour() {
+    const { t } = useTranslation('home');
+    // {t("key")}
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    return (
+        <div className={'site-wrapper'}>
+            <div className={'header-blog'}>
+                <Navbar />
+                {/* <!-- ====== 1.2 hero section ====== --> */}
+                <section className="hero">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-lg-11 col-md-12 text-center" data-aos="fade">
+                                <div>
+                                    <h2>{t('our blogs')}</h2>
+                                    <div className="d-flex justify-content-center gap-4 pt-4">
+                                        <nav aria-label="breadcrumb">
+                                            <div className="breadcrumb d-flex gap-4 align-items-center">
+                                                <div className="breadcrumb-item">
+                                                    <Link to="/">{t('home')}</Link>
+                                                </div>
+                                                <FontAwesomeIcon icon={faAnglesRight} />
+                                                <div className="breadcrumb-item" aria-current="page">
+                                                    {t('blog')}
+                                                </div>
+                                            </div>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* <!-- ====== End 1.2 hero section ====== --> */}
+            </div>
+        </div>
+    );
+}
+
+export default SingleTour;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,43 +1,47 @@
 // import PAGE
-import Home from "~/pages/Home";
-import About from "~/pages/About";
-import Blog from "~/pages/Blog";
-import Contact from "~/pages/Contact";
-import Following from "~/pages/Following";
-import Profile from "~/pages/Profile";
-import Upload from "~/pages/Upload";
-import Page404 from "~/pages/Page404";
-import Login from "~/pages/Login";
-import Signup from "~/pages/Signup";
-import Test from "~/pages/Test";
-import Destinations from "~/pages/Destinations";
-import Services from "~/pages/Services";
-import Testimonials from "~/pages/Testimonials";
-import Team from "~/pages/Team";
-import FAQ from "~/pages/FAQ";
+import Home from '~/pages/Home';
+import About from '~/pages/About';
+import Blog from '~/pages/Blog';
+import Contact from '~/pages/Contact';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Page404 from '~/pages/Page404';
+import Login from '~/pages/Login';
+import Signup from '~/pages/Signup';
+import Test from '~/pages/Test';
+import Destinations from '~/pages/Destinations';
+import Services from '~/pages/Services';
+import Testimonials from '~/pages/Testimonials';
+import Team from '~/pages/Team';
+import FAQ from '~/pages/FAQ';
+import SinglePost from '~/pages/SinglePost';
+import SingleTour from '~/pages/SingleTour';
 
 // import LAYOUT
-import { HeaderOnly } from "~/Layout";
-
+import { HeaderOnly } from '~/Layout';
 
 const publicRoutes = [
-  { path: "/", component: Home },
-  { path: "/about", component: About },
-  { path: "/blog", component: Blog },
-  { path: "/contact", component: Contact },
-  { path: "/services", component: Services },
-  { path: "/destinations", component: Destinations },
-  { path: "/testimonials", component: Testimonials },
-  { path: "/team", component: Team },
-  { path: "/faq", component: FAQ },
-  { path: "/following", component: Following },
-  { path: "/profile", component: Profile, layout: null },
-  { path: "/upload", component: Upload, layout: HeaderOnly },
-  { path: "*", component: Page404, layout: null },
-  { path: "/login", component: Login, layout: null },
-  { path: "/signup", component: Signup, layout: null },
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/blog', component: Blog },
+    { path: '/contact', component: Contact },
+    { path: '/services', component: Services },
+    { path: '/destinations', component: Destinations },
+    { path: '/testimonials', component: Testimonials },
+    { path: '/team', component: Team },
+    { path: '/faq', component: FAQ },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '*', component: Page404, layout: null },
+    { path: '/login', component: Login, layout: null },
+    { path: '/signup', component: Signup, layout: null },
 
-  { path: "/test", component: Test, layout: null },
+    { path: '/test', component: Test, layout: null },
+
+    { path: '/single-post', component: SinglePost },
+    { path: '/single-tour', component: SingleTour },
 ];
 
 const privateRoutes = [];
