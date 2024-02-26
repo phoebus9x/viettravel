7e03860c38a6ceec18c914d232694bf5f8a5b5db
phoebus9x <anhtu.coder0512@gmail.com>
2024-02-26 13:35:27 +0300

    update 26/02 singleBlog

--- src/pages/testPost/index.js
+++ src/pages/testPost/index.js
@@ -1,28 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './testPost.scss';
 import { useSelector } from 'react-redux';
 function TestPost() {
-    const [data, setData] = useState('');
-
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
-
-    // const fetchData = async () => {
-    //     const id = '65d5f426577e4f9ec4a866a7';
-    //     axios
-    //         .get(`http://localhost:1110/v1/api/blog/${id}`)
-    //         .then((response) => {
-    //             setData(response.data.data.content);
-    //             // Xử lý dữ liệu được lấy về ở đây
-    //             // console.log('blogData: ', response.data.data.content);
-    //         })
-    //         .catch((error) => {
-    //             console.error('Error fetching blog:', error);
-    //             // Xử lý lỗi ở đây
-    //         });
-    // };
 
     const blogState = useSelector((state) => state.blog.blog)
     console.log("blogState: ", blogState)
